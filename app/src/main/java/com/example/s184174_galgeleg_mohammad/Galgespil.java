7c596b02c9a569ccf02aeee100118becb8de879c
Nafiu <s184174@student.dtu.dk>
2020-10-31 18:24:25 +0100

    Videreudvikling i UI til galgelogikken

--- app/src/main/java/com/example/s184174_galgeleg_mohammad/Galgespil.java
+++ app/src/main/java/com/example/s184174_galgeleg_mohammad/Galgespil.java
@@ -1,12 +1,19 @@
 package com.example.s184174_galgeleg_mohammad;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Galgespil extends AppCompatActivity {
-    Galgelogik galgelogik = new Galgelogik();
+public class Galgespil extends AppCompatActivity implements View.OnClickListener {
+    private Galgelogik galgelogik = new Galgelogik();
+    private EditText input;
+    private TextView ord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +21,24 @@ public class Galgespil extends AppCompatActivity {
         setContentView(R.layout.galgespil);
 
         TextView besked = findViewById(R.id.Besked);
-        TextView ord = findViewById(R.id.Ordet);
+
+        ImageView galge = findViewById(R.id.Galge);
+        Button gaet = findViewById(R.id.button);
+        input = findViewById(R.id.InputBogstav);
+        ord = findViewById(R.id.Ordet);
+
 
         besked.setText(R.string.Besked);
         ord.setText(galgelogik.getSynligtOrd());
+        galge.setImageResource(R.drawable.galge);
+        gaet.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        galgelogik.gætBogstav(input.getText().toString());
+        ord.setText(galgelogik.getSynligtOrd());
+        input.setText("");
+    }
 }
